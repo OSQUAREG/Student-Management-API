db30bb5b3dcaafdef9d3c544e3f6e1f21432476d
Gregory Ogbemudia <gregory.ogbemudia@gmail.com>
2023-03-18 01:53:09 +0100

    created a function to create defaults

---
+++ create_defaults.py
@@ -0,0 +1,61 @@
+from decouple import config
+from .api.models import User, Department, GradeScale
+from werkzeug.security import generate_password_hash
+from datetime import datetime
+
+def create_defaults():
+    """Run after Table Creation"""
+    super_admin = User(
+        first_name="Super",
+        last_name="Admin",
+        gender="MALE",
+        email="superadmin@sm.com",
+        username="super.admin",
+        password_hash=generate_password_hash(config("DEFAULT_SUPERADMIN_PASSWORD")),
+        type="user",
+        department_id=1,
+        created_on=datetime.utcnow()
+        is_staff=False,
+        is_admin=True
+    )
+    super_admin.save_to_db()
+
+    admin = User(
+        first_name="Admin",
+        last_name="Admin",
+        gender="MALE",
+        email="admin@sm.com",
+        username="admin",
+        password_hash=generate_password_hash(config("DEFAULT_ADMIN_PASSWORD")),
+        type="user",
+        department_id=1,
+        created_on=datetime.utcnow()
+        is_staff=False,
+        is_admin=True
+    )
+    admin.save_to_db()
+
+    admin_department = Department(
+        name="Admins",
+        code="ADMS",
+        created_by="super.admin",
+        created_on=datetime.utcnow()     
+    )
+    admin_department.save_to_db()
+
+    grade = ["A", "B", "C", "D", "E", "F"]
+    point = [4, 3, 2, 1, 0, 0]
+    min = [70, 60, 50, 45, 40, 0]
+    max = [100, 69, 59, 49, 44, 39]
+    i = 0
+    while i < len(grade):
+        gradescale = GradeScale(
+            grade=grade[i],
+            point=point[i],
+            min=min[i],
+            max=max[i],
+            created_by="super.admin",
+            created_on=datetime.utcnow()
+        )
+        gradescale.save_to_db()
+        i += 1
